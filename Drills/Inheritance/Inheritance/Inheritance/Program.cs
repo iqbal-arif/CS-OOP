9d65e8ffcd508348dc0d3f1d18826c3200b5c05c
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-15 23:51:46 -0600

    Inheritance and Employment code initial commit

---
+++ Drills/Inheritance/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,52 @@
+﻿using Inheritance;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Inheritance!");
+        Console.WriteLine("Inheritance Implementation in Zoo Project!\n");
+
+        //Animal cat = new Animal(4.5);
+        //Animal dog = new Animal(10);
+
+        Cat cat = new Cat();
+        Dog dog = new Dog(10);
+
+        Console.WriteLine(cat.Weight);
+
+    }
+}
+
+namespace Inheritance
+{
+    abstract class Animal
+    {
+        public Animal(double weight)
+        {
+            Weight = weight;
+        }
+
+        public double Weight { get; set; }
+    }
+
+
+    class Cat : Animal
+    {
+        //The Constructor will not run until it calls the Base constructor to get the weight of the Cat
+        public Cat() : base(4.5)
+        {
+        }
+
+    }
+
+    class Dog : Animal
+    {
+        //Constructor has two option take the external value or call the base for the weight value.
+        public Dog(double weight) : base(weight)
+        {
+
+        }
+
+    }
+}
---
+++ Drills/Practice/Employment/Employment/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Employment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Employment!\n");
+
+            Employee employeeOne = new Employee();
+            Consultant consultantOne= new Consultant(45, 250);
+
+            Console.WriteLine(employeeOne.Name);
+            Console.WriteLine(consultantOne.Name);
+            Console.WriteLine(consultantOne.HourlyRate);
+            Console.WriteLine(consultantOne.HoursReported);
+        }
+    }
+
+    class Person
+    {
+        public Person(string name)
+        {
+            Name = name;
+        }
+        public string Name { get; set; }
+    }
+
+    class Employee : Person
+    {
+        public Employee() : base("Arif")
+        {
+            Console.WriteLine(Name);
+        }
+        public string Department { get; set; }
+    }
+
+    class Consultant : Person
+    {
+        public Consultant(int hourRate, int hourReport) : base("Arif")
+        {
+            Console.WriteLine(Name);
+            Console.WriteLine(HourlyRate);
+            Console.WriteLine(HoursReported);
+        }
+        public decimal HourlyRate { get; set; }
+        public int HoursReported { get; set; }
+    }
+}
