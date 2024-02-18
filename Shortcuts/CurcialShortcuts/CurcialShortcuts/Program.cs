b944c417e945a1f7f13de52f4d4ba7d95248f72b
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-17 22:17:08 -0600

    CuricialShortcut initial commit and debugging gif added

---
+++ Shortcuts/CurcialShortcuts/CurcialShortcuts/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace CurcialShortcuts
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Curcial Shortcuts\n!");
+
+            MethodShortcut();
+        }
+
+        private static void MethodShortcut()
+        {
+            
+        }
+    }
+}
