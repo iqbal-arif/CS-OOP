7f102b4d732689bd93ae5b3529831ee87ec608e0
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-18 21:45:40 -0600

    Debug Shortcuts Initial commit

--- Shortcuts/CurcialShortcuts/CurcialShortcuts/Program.cs
+++ Shortcuts/CurcialShortcuts/CurcialShortcuts/Program.cs
@@ -1,4 +1,5 @@
 ﻿
+
 namespace CurcialShortcuts
 {
     internal class Program
@@ -6,8 +7,21 @@ namespace CurcialShortcuts
         static void Main(string[] args)
         {
             Console.WriteLine("Curcial Shortcuts\n!");
-
+            Console.WriteLine();
+            
             MethodShortcut();
+
+            MethodWithParameter("Shortcut");
+        }
+
+        private static void MethodWithParameter(string v)
+        {
+            Console.WriteLine("ALT+Enter Key for Options");
+            Console.WriteLine("F9 Key for Debug Mark");
+            Console.WriteLine("F5 Key for Debugging");
+            Console.WriteLine("F11 Key for STEP-IN");
+            Console.WriteLine("SHITF + F11 Key for STEP-OUT");
+            Console.WriteLine("SHITF + F10 Key for STEP-OVER");
         }
 
         private static void MethodShortcut()
