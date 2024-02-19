76d80bab99c78f5f37cce98877eae448b10b5825
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-19 11:00:21 -0600

    BeeHive Initial Commit

---
+++ Drills/Practice/BeeHIve/BeeHive/BeeHive/BeeHive.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeeHive
+{
+     class BeeHive
+    {
+        public BeeHive(QueenBee queen)
+        {
+            Queen = queen;
+            Drones = new List<DroneBee>();
+        }
+
+        //Properties
+        public QueenBee Queen { get; set; }
+
+        public List<DroneBee> Drones { get; set; }
+    }
+
+    class Bee
+    {
+        public Bee(string name)
+        {
+            Name = name;
+        }
+
+        //Properties
+        public string Name { get; set; }
+    }
+
+    class QueenBee : Bee
+    {
+        public QueenBee(string name) : base(name) 
+        {
+            
+        }
+    }
+
+    class DroneBee : Bee
+    {
+        public DroneBee(string name) : base(name) 
+        {
+            
+        }
+        //Properties
+        public int HoneyCount { get; set; }
+    }
+}
---
+++ Drills/Practice/BeeHIve/BeeHive/BeeHive/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace BeeHive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("BeeHive!\n");
+
+            BeeHive hive = new BeeHive(new QueenBee("Queen"));
+
+            hive.Drones.Add(new DroneBee("Buzzy"));
+            hive.Drones.Add(new DroneBee("Fuzzy"));
+            hive.Drones.Add(new DroneBee("Kuzzy"));
+          
+
+        }
+    }
+}
