e316854e291163995738e6e961ac1aedfffbabc5
Arif Iqbal <arif_iqbal@yahoo.com>
2024-02-18 23:13:09 -0600

    Dictionary Listing Initial Commit

--- Concepts/Generic/Collections/Collections/Program.cs
+++ Concepts/Generic/Collections/Collections/Program.cs
@@ -17,7 +17,38 @@
             {
                 Console.WriteLine(names[i]);
             }
+            
+            //Code wiating for the longest in the Queue
 
+            var queue = new Queue<string>();
+
+            queue.Enqueue("One");
+            queue.Enqueue("Two");
+            queue.Enqueue("Three");
+
+            Console.WriteLine("\nList the long waited Item in the Queue!\n");
+            Console.WriteLine(queue.Dequeue() + "\n");
+
+            //Stack list the last itme first in the Queue
+
+            var stack = new Stack<int>();
+
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+
+            Console.WriteLine("\nList the Last Item listed in the Queue Stack!\n");
+            Console.WriteLine(stack.Pop() + "\n");
+
+            //Dictionary Key & Value List
+
+            var student = new Dictionary<string, int>();
+
+            student.Add("Arif", 47);
+            student.Add("Jack", 32);
+            student.Add("Joe", 40);
+
+            Console.WriteLine(student["Arif"]);
             Console.ReadLine();
         }
     }
